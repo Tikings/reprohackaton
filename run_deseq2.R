f6194703bea03bf0079888d9dd95dc3304cf26d6
Vincent <vincent.fourmigue@agroparistech.fr>
2024-11-21 14:23:14 +0100

    ajout des noms via un .tsv plus que les pathwys KEGG à faire

--- run_deseq2.R
+++ run_deseq2.R
@@ -2,11 +2,13 @@ if (!require("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 }
 
+
+
+
 # Install Bioconductor packages
 BiocManager::install("DESeq2")
 BiocManager::install("tximport")
 BiocManager::install("apeglm")
-BiocManager::install("biomaRt")
 
 library(ggplot2)
 library(DESeq2)
@@ -22,13 +24,11 @@ library(biomaRt)
 
 
 
-count_data <- as.matrix(read.table("counts_final.txt", 
+count_data <- as.matrix(read.table("./counts_final.txt", 
                                    header = TRUE, 
                                    row.names = 1, 
                                    sep = "\t", 
                                    skip = 1)[, -c(1:5)])
-
-#print(count_data)
 
 
 sample_names <- colnames(count_data)
@@ -44,107 +44,80 @@ sample_conditions <- c(
 
 col_data <- data.frame(
   row.names = sample_names,
-  condition = sample_conditions[sample_names] )
+  condition = factor(sample_conditions[sample_names]) )
 
-#print(col_data)
-
-
-dds <- DESeqDataSetFromMatrix(countData = count_data, colData = col_data, design = ~ condition)
+dds <- DESeqDataSetFromMatrix(countData = count_data,
+                              colData = col_data
+                              , design = ~ condition)
 
 dds <- DESeq(dds)
 res <- results(dds)
-summary(res)
-
-
-res <- lfcShrink(dds, coef="condition_persister_vs_control", type="apeglm") 
-
-plotMA(res, ylim=c(-5,5))
-
-
-
+res
+#plotMA(res, ylim=c(-4,4))
 
 
 res_df <- as.data.frame(res)
-res_df$significant <- ifelse(res_df$padj < 0.05, "Significant", "Non-significant")
+res_df$significant <- ifelse(res_df$padj < 0.05,
+                             "Significant",
+                             "Non-significant")
 
-print(res_df)
 
-# Create the customized MA plot
 ggplot(res_df, aes(x = baseMean, y = log2FoldChange)) +
-  geom_point(aes(color = significant), size = 1.0, alpha=0.5) + # Adjust point size as needed
+  geom_point(aes(color = significant), size = 1.0, alpha=0.5) + 
   scale_color_manual(values = c("Significant" = "red","Non-significant" = "black")) +
-  scale_x_log10() +  # Log scale for base mean
-  geom_hline(yintercept = 0, linetype = "dashed") +  # Add a dashed line at y = 0
-  theme_minimal() +  # Use a minimal theme for a clean look
+  scale_x_log10() +  
+  geom_hline(yintercept = 0, linetype = "dashed") +  
+  theme_minimal() +  
   labs(x = "Mean of normalized counts", y = "Log2 fold change") +
   theme(
-    legend.position = "none",  # Remove legend if not needed
-    panel.grid.minor = element_blank()  # Clean up minor grid lines
-  ) +
-  ylim(c(-4, 4)) # Set the y-axis limits
+    legend.position = "none",  
+  ) + ylim(c(-4, 4)) 
+
+
+ggsave("MA_plot_1.png")
+
+
+
+
+
+# pas de lfc shrink du tout ? l'image sans semble bcp plus proche de celle du papier que celle avec
+
+#res <- lfcShrink(dds, coef="condition_persister_vs_control", type="apeglm") 
+# quel type utiliser : apeglm, ashr, normal ?
+
 
 ##### à suivre 
 
+library(readr)
+library(tidyverse)
+
+gene_annotations <- read_csv("NCTC8325.csv", show_col_types = FALSE)
+
+gene_annotations <- gene_annotations %>%
+  separate(
+    col = `locus tag;pan gene symbol;symbol;synonym;Gene ID` ,
+    into = c("locus_tag", "pan_gene_symbol", "symbol", "synonym", 
+             "Gene_ID"),
+    sep = ";",
+    fill = "right"
+  )
 
 
-#  biomaRt  fait le pont entre l'ID Ensembl et l'annotation des gènes
-ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "aureowiki_gene")
-gene_annotations <- getBM(attributes = c("ensembl_gene_id", "external_gene_name", "description"),mart = ensembl)
+
+
 
 res <- as.data.frame(res)
-res <- left_join(res, gene_annotations, by = c("row.names" = "ensembl_gene_id"))
 
+res <- res %>% rownames_to_column(var = "row.names")
 
-# KEGG Translation genes
-# est ce que je dois prendre tous les gènes de translation ?
-kegg_translation <- GET("http://rest.kegg.jp/link/aureowiki/translation")
-kegg_translation_genes <- content(kegg_translation, as = "text")
-kegg_translation_genes <- read.table(text = kegg_translation_genes, sep="\t", header = FALSE)
+res$row.names <- sub("^gene-", "", res$row.names)
 
-translation_genes <- unique(kegg_translation_genes$V2)
+res
 
-# ajout d'une colonne avec vrai faux si gène à encercler
-res$translation_gene <- ifelse(res$Row.names %in% translation_genes, TRUE, FALSE)
+res <- left_join(res, gene_annotations, by = c("row.names" = "locus_tag"))
 
+res
 
-summary(res)
-
-
-
-
-
-# modification potentielle des seuils optionelle,
-#il faut chercher quelles combinaisons ont été utilisées dans l'article 
-#seuil pour lfcThreshold
-res.05 <- results(dds, alpha = 0.05)
-table(res.05$padj < 0.05)
-
-#seuil pour padj
-resLFC1 <- results(dds, lfcThreshold=1)
-table(resLFC1$padj < 0.1)
-
-
-resultsNames(dds)
-
-
-#coef names à changer
-# quel type utiliser : apeglm, ashr, normal ?
-res <- lfcShrink(dds, coef="condition_persister_vs_control", type="apeglm") 
-
-plotMA(res, ylim=c(-5,5))
-#ggsave("MA_plot.png")
-
-
-ggplot(as.data.frame(res), aes(x = log2(baseMean), y = log2FoldChange, color = padj < 0.05)) +
-  geom_point(aes(shape = translation_gene), size = 2) +
-  scale_color_manual(values = c("grey", "red")) +
-  scale_shape_manual(values = c(19, 1)) +  
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  theme_minimal() +
-  geom_text_repel(aes(label = ifelse(Row.names %in% c("frr", "infA", "infC", "infB", "tsf", "pth"), external_gene_name, "")),
-                  color = "black", size = 3)
-# récupéreration des noms à la main on peut faire autrement ? 
-ggsave("MA_plot_enhanced.png")
-
+## plus que KEGG à faire 
 
 
